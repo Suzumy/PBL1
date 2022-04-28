a3c7c9dd4ffe39f0ed44d8af87b747ec7e3b0619
takahiro-ochi <86271943+takahiro-ochi@users.noreply.github.com>
2022-04-28 09:30:57 +0900

    Add files via upload

---
+++ dbdata/dbsql.php
@@ -0,0 +1,31 @@
+<?php
+    require_once __DIR__.'/dbdata.php';
+
+    //ユーザー表示
+    class User extends DbData{
+        public function authUser($articleId){
+            $sql = "SELECT article.articleId,users.userName,users.imagepath,article.title,article.explanation,article.articleimg1,article.articleimg2 FROM users,article WHERE users.userId=article.userId AND article.articleId = ?";
+            $stmt = $this->query($sql,[$articleId]);
+            return $stmt->fetch();
+        }
+
+        public function allarticle(){
+            $sql = "SELECT article.articleId,title,users.userName,users.imagepath FROM article,users WHERE users.userId=article.userId;";
+            $stmt = $this->query($sql,[]);
+            return $stmt->fetchAll();
+        }
+
+        //ログイン認証
+        public function certification($userId, $password){
+            $sql = "SELECT * FROM users WHERE address = ? and password = ?";
+            $stmt = $this->query($sql, [$userId, $password]);
+            return $stmt->fetch();
+        }
+
+        //コメントをDBに保存する
+        public function comment_storage($articleId, $userId, $comment){
+            $sql = "INSERT INTO comment(articleId, userId, comment) VALUES(?, ?, ?)";
+            $result = $this->exec($sql, [$articleId, $userId, $comment]);
+        }
+    }
+?>
