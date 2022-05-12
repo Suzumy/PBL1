a3976af920745b49c3afe78316470fd0899a5fa7
たか <kd1277071@st.kobedenshi.ac.jp>
2022-05-12 10:44:04 +0900

    detail.phpに変更があります。

--- detail.php
+++ detail.php
@@ -1,13 +1,3 @@
-
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>投稿記事一覧</title>
-    <link rel="stylesheet" type="text/css" href="css/detail.css">
-</head>
-
-
 <?php
 
     session_start();
@@ -16,10 +6,13 @@
     $user = new User();
 
     $data = $_GET['data'];
-    
+
     $users = $user->authUser($data[0]);
-    
-    
+
+    $_SESSION['articleId'] = $data[0];
+
+    $get_comment = $user->get_comment($_SESSION['articleId']);
+
 ?>
 <head>
     <title>投稿記事一覧</title>
@@ -40,7 +33,7 @@
 
     <ul style="list-style-type: circle;">
     </ul>
-    <table style="border-collapse: collapse; width: 1045px;" border="1">
+    <table style="border-collapse: collapse; width: 100%;" border="1">
         <tbody>
             <tr>
                 <td style="width: 100%;">
@@ -48,25 +41,41 @@
                     <p style="text-align: left; padding-left: 40px;"><?= $users['explanation'] ?></p>
                     <img src=" images/<?= $users['articleimg1'] ?>" alt="アイコン" width="100" height="100">
                     <img src=" images/<?= $users['articleimg2'] ?>" alt="アイコン" width="100" height="100">
-                    <br>
-                    <?php
-                        if($users['urlpath'] != null){
-                    ?> 
-                        <a href="<?php echo $users['urlpath'] ?>"><?php echo $users['urlpath'] ?></a> 
-                    <?php
-                        }
-                    ?>
                 </td>
             </tr>
         </tbody>
     </table>
     <button type="button">👍&nbsp;</button><button type="button">&nbsp;👎</button>
+
     <p style="margin-right: 1000px">コメント</p>
+        <form method="POST" action="comment_db.php" target="sendPhoto">
             <textarea name="comment" style="width: 1045px; height: 300px;" cols="50" rows="1" 
             maxlength="1000"></textarea>
     <div class="btn">
         <button class="Form-Btn pos" onclick="location.href='#'">送信</button>
     </div>
+    </form>
+    <iframe name="sendPhoto" style="width:0px;height:0px;border:0px;"></iframe>
+
+    <?php
+        foreach($get_comment as $row){//コメント表示
+    ?>
+
+    <section>
+        <button onclick="location.href='detail.php?data%5b%5d=<?= $row['articleId']?>'" target='_blank' class='btn_ao_a'>
+            <span class="a__icon">
+                    <img width="20px" src="./images/<?= $row['imagepath'] ?>" alt="<?= $row['userName'] ?>">
+            </span>
+            <span>
+                <a id="nametag" href="profile.php"><?= $row['userName'] ?></a>
+            </span>
+        </button>
+            <span class="comment">
+                <?= $row['comment'] ?>
+            </span>
+        
+    </section>
+    <?php } ?>
 </body>
 
 </html>
