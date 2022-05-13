f394be41fdbf5171a1668baa6c1cefacbc6db6da
benibenisan <72596397+benibenisan@users.noreply.github.com>
2022-05-13 11:24:31 +0900

    プロフィール画面に固定の情報

--- profile.php
+++ profile.php
@@ -5,6 +5,8 @@
 		<title>プロフィール</title>
 		<link rel="stylesheet" type="text/css" href="css/profile.css">
 	</head>
+    <body class="wrap">
+	
 	<?php
 		require_once __DIR__ . './header.php';
 		require_once __DIR__.'/./dbdata/dbsql.php';
@@ -15,14 +17,15 @@
 		$profile = $user->detailuser($data[0]);
 
 	?>
-    <body class="wrap">
         <div class="content">
             <h1 class="heading-lv1 text-center">プロフィール</h1>
 			<figure class="profile-image">
                 <img src="./images/<?= $profile['imagepath'] ?>" width="300" height="300">
 			</figure>
+			
             <h2 class="heading-lv2 heading-margin text-center"><?= $profile['userName'] ?></h2>
-
+			<h2 class="heading-lv2 heading-margin text-center">４年　ITエキスパート学科</h2>
+		
             <p class="text text-center"><?= $profile['profile'] ?></p>
 
 
