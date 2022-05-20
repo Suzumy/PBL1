4f8e6e280b7d9080459587ab4fcfae6f3413c317
taichi626 <kd1263234@st.kobedenshi.ac.jp>
2022-05-20 11:17:54 +0900

    ファイル結合　コメント

--- ORnum.php
+++ ORnum.php
@@ -1,6 +1,6 @@
 <?php
     //送られてきたデータを受け取る
-    $answer = $_POST['answer'];
+    $comment = $_POST['comment'];
     $articleId = $_SESSION['articleId'];
 
     //userオブジェクトを生成する
@@ -10,15 +10,15 @@
     session_start();
 
     //空白消す
-    $answer = preg_replace( '/\A[\p{C}\p{Z}]++|[\p{C}\p{Z}]++\z/u', '', $answer);
+    $comment = preg_replace( '/\A[\p{C}\p{Z}]++|[\p{C}\p{Z}]++\z/u', '', $comment);
     //改行消す
-    $answer = str_replace(array("\r\n","\n","\r"), '', $answer);
+    $comment = str_replace(array("\r\n","\n","\r"), '', $comment);
 
     $max_len = 160;
-    $len = strlen($answer);
+    $len = strlen($comment);
 
     //空白、改行、その他エラー判定
-    if($answer == ''){
+    if($comment == ''){
         $_SESSION['text_error'] = '入力してから送信ボタンを押してください。';
         header('Location: detail.php?data%5b%5d='+$articleId);
         exit();
@@ -30,4 +30,9 @@
         header('Location: detail.php?data%5b%5d='+$articleId);
         exit();
     }
-    $ans_storage = $user->ans_storage($_SESSION['articleId'], $_SESSION['userId'], $answer);
+
+    if($_SESSION['ORnum'] == 1){
+        $comment_storage = $user->comment_storage($_SESSION['articleId'], $_SESSION['userId'], $comment);
+    }elseif($_SESSION['ORnum'] == 2){
+        $ans_storage = $user->ans_storage($_SESSION['articleId'], $_SESSION['userId'], $comment);
+    }
--- detail.php
+++ detail.php
@@ -33,7 +33,7 @@ $users = $user->authUser($data[0]);
 $_SESSION['articleId'] = $data[0];
 
 
-$get_comment = $user->get_comment($_SESSION['articleId']);
+//$get_comment = $user->get_comment($_SESSION['articleId']);
 ?>
 
 <body bgcolor="#d9ead3" style="padding: 0;">
@@ -57,7 +57,6 @@ $get_comment = $user->get_comment($_SESSION['articleId']);
         <tbody>
             <tr>
                 <td style="width: 100%;">
-
                     <p style="text-align: left; padding-left: 40px;"><?= h($users['title']) ?></p>
                     <textarea disabled style="border: none; background-color: #d9ead3; color: inherit; width: 900px; height: 300px;" name=”テキストエリア” rows=”3″ cols=”50″ wrap=”hard”><?= h($users['explanation']) ?></textarea>
                     <?php
@@ -89,12 +88,17 @@ $get_comment = $user->get_comment($_SESSION['articleId']);
     </div>
 
     <?php
+    $_SESSION['ORnum'] = $users['ORnum'];
     //詳細が製作物の時
-    if ($users['ORnum'] == 1) {
-        $get_comment = $user->get_comment($_SESSION['articleId']);
+    if ($_SESSION['ORnum'] == 1) {
+        $get_comment = $user->get_comment($_SESSION['articleId'],$_SESSION['ORnum']);
+        echo '<p style="margin-right: 1000px">コメント</p>';
+    }elseif ($_SESSION['ORnum'] == 2){
+        $get_answers = $user->get_answers($_SESSION['articleId']);
+        echo '<p style="margin-right: 1000px">質問回答</p>';
+    }
     ?>
-        <p style="margin-right: 1000px">コメント</p>
-        <form id="comment_form" method="POST" action="comment_db.php" target="sendPhoto">
+        <form id="comment_form" method="POST" action="ORnum.php" target="sendPhoto">
             <textarea id="comment" name="comment" style="width: 1045px; height: 300px;" cols="50" rows="1" maxlength="1000" onkeyup="commentlen();"></textarea>
             <div id="comment_num">0/160</div>
             <?php
@@ -111,8 +115,13 @@ $get_comment = $user->get_comment($_SESSION['articleId']);
         <iframe name="sendPhoto" style="width:0px;height:0px;border:0px;"></iframe>
 
         <?php
+        if($_SESSION['ORnum']==1){
+            $line_up = $get_comment;
+        }elseif($_SESSION['ORnum']==2){
+            $line_up = $get_answers;
+        }
         //コメント表示
-        foreach ($get_comment as $row) {
+        foreach ($line_up as $row) {
             $url_param = url_param_change(array("userId" => $row['userId']))
         ?>
             <section>
@@ -125,55 +134,17 @@ $get_comment = $user->get_comment($_SESSION['articleId']);
                     </span>
                 </button>
                 <span class="comment">
-                    <?= h($row['comment']) ?>
+                    <?php
+                        if($_SESSION['ORnum']==1){
+                            echo h($row['comment']);
+                        }elseif($_SESSION['ORnum']==2){
+                            echo h($row['answer']);
+                        }
+                    ?>
                 </span>
             </section>
         <?php
         }
-    }
 
-    //詳細が質問のとき
-    if ($users['ORnum'] == 2) {
-        $_SESSION['ORnum'] = $users['ORnum'];
-        $get_answers = $user->get_answers($_SESSION['articleId']); ?>
-        <p style="margin-right: 1000px">質問回答</p>
-        <form method="POST" action="ORnum.php" target="sendPhoto">
-            <textarea id="comment" name="answer" style="width: 1045px; height: 300px;" cols="50" rows="1" maxlength="1000" onkeyup="commentlen();"></textarea>
-            <div id="comment_num">0/160</div>
-            <?php
-                //入力エラーがあるならば表示
-                if(isset($_SESSION['text_error'])){
-                    echo '<p class="error_class" style="color:red;">'. $_SESSION['text_error'].'</p>';
-                    unset($_SESSION['text_error']);
-                }
-            ?>
-            <div class="btn">
-                <button type="submit" class="Form-Btn pos" onclick="comment_update()">送信</button>
-            </div>
-        </form>
-        <iframe name="sendPhoto" style="width:0px;height:0px;border:0px;"></iframe>
-
-        <?php
-        //コメント表示
-        foreach ($get_answers as $row) {
-            $url_param = url_param_change(array("userId" => $row['userId']))
-        ?>
-            <section>
-                <button onclick="location.href='transition_profile.php?<?php echo $url_param; ?>'" target='_blank' class='btn_ao_a'>
-                    <span class="a__icon">
-                        <img width="20px" src="./images/<?= $row['imagepath'] ?>" alt="<?= h($row['userName']) ?>">
-                    </span>
-                    <span>
-                        <a id="nametag"><?= h($row['userName']) ?></a>
-                    </span>
-                </button>
-                <span class="answer">
-                    <?= h($row['answer']) ?>
-                </span>
-
-            </section>
-    <?php
-        }
-    }
     ?>
 </body>
